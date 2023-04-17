a72c0ae973cef65336beb3c675c12c96fa784e9b
RicardoSilvaF <zanphs@gmail.com>
2023-04-16 21:12:55 -0300

    Implementacao do post 'participants'

--- src/app.js
+++ src/app.js
@@ -3,12 +3,68 @@ import joi from "joi";
 import cors from "cors";
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
+import dayjs from "dayjs";
+
+dotenv.config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-dotenv.config();
+
+const mongoClient = new MongoClient(process.env.DATABASE_URL);
+let db;
+
+try{
+    await mongoClient.connect();
+    db = mongoClient.db("batepapouol");
+    console.log(`conectou no banco`)
+}
+catch(error){
+    console.log(error);
+    console.log("não conectou no banco de dados");
+}
+
+// SCHEMAS
+const participantsSchema = joi.object({
+    name: joi.string().required().min(1)
+})
+//
+
+app.post("/participants", async (req,res) => {
+    const { name } = req.body;
+    const { error } = participantsSchema.validate({ name });
+
+    if(error){
+        console.log("nao passou na validacao")
+        return res.sendStatus(422)
+    }
+    
+    try{
+        let participantsCheck = await db.collection("participants").findOne({ name });
+        if(participantsCheck){
+            console.log("usuario já existe")
+            res.sendStatus(409);
+            return;
+        }
+        let date = Date.now();
+        await db.collection("participants").insertOne({name: name , lastStatus: date});
+        await db.collection("messages").insertOne({ 
+            from: name,
+            to: 'Todos',
+            text: 'entra na sala...',
+            type: 'status',
+            time: dayjs(date).format("HH:MM:SS")
+        });
+        res.status(201).send("cadastrado com sucesso")
+        return;
+    }
+    catch(error){
+        console.log(error)
+        res.status(500).send("Houve um erro")
+        return;
+    }
+});
 
 const port = 5000;
-app.listen(port);
+app.listen(port, () => console.log('server ligado'));
